881f7cbfdefff98bd49939b79342b27e02bba19f
Penina Wanjiru <penina.wanjiru@andela.com>
2016-03-23 23:53:32 +0300

    added number of chances user has

--- main.py
+++ main.py
@@ -1,38 +1,50 @@
 import random
 
-
-def get_words():
-
+def get_w():
+    """Get words from a dictionary and checks if they match what the\
+    user entered"""
     words = {
-            'AFRICA':['A','F','R','I','C','A'],
-            'ASIA': ['A','S','I','A'],
-            'NOTEBOOK':['N','O','T','E','B','O','O','K'],
-            'FILTER':['F','I','L','T','E','R'],
-            'DOJO':['D','O','J','O'],
-            'ANDELA':['A','N','D','E','L','A'],
-            'PYTHON':['P','Y','T','H','O','N'],
-            'HOPE':['H','O','P','E'],
-            'WORDS':['W','O','R','D','S']
+            "PENNY":"Name of a girl",
+            "ANDELA":"A technology company that trains developers",
+            "AFRICA":"A continent",
+            "EUROPE":"A continent",
+            "GERMANY":"A country"
             }
-
-    new_word = words[random.choice(words.keys())]
-    length = len(new_word)
-    results = list('_' * length)
+    # Returns all keys in the dictionary in a list
+    specific_words = words.keys()
+    # Selects a random key from the dictionary
+    key = random.choice(specific_words)
+    # Gets the value of the corresponding key
+    description = words[key]
+    print 'Clue: %s' % description
+    length = len(key)
+    results = list('_'*length)
     print ''.join(results)
-
-    for tries in range(9):
-        answer = raw_input('Enter a letter ')
+    # Initializes the number of chances the player has.
+    chance_count = 9
+    answer_letters = []
+    while chance_count > 0:
+        answer = raw_input("enter a letter:").upper()
+        if answer in answer_letters:
+            print "You have tried letter %s already." % (answer)
+            continue
+        answer_letters.append(answer)
+        # Converts the key into a list for easy comparison with results.
+        keys = list(key)
+        flag = False
+        # Replaces the found letter with the one inputed by the user.
         for index in range(length):
-            if answer == new_word[index]:
+            if keys[index] == answer:
                 results[index] = answer
-
-        if results == new_word:
+                flag = True
+        # Checks if the final word is equal to the word.
+        if results == keys:
+            print ''.join(results)
+            print "Congratulations! You got it!"
             break
-
         print ''.join(results)
+        if flag is not True:
+            chance_count -= 1
+        print "You have %d chances left" % chance_count
 
-
-
-    # print results
-
-get_words()
+get_w()
